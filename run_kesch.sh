06ffb1a2ff284766582a008af8191fc7e41f8891
Pascal Spörri <pascal.spoerri@env.ethz.ch>
2017-02-07 12:16:23 +0100

    Updated runscript

--- run_kesch.sh
+++ run_kesch.sh
@@ -2,7 +2,7 @@
 module purge
 source modules_kesch.env
 [ -z "${jobs}" ] && jobs=2
-echo Running with ${jobs} 
-export G2G=2
+echo "Running with ${jobs} jobs"
+[ -z "${G2G}" ] && export G2G=2
 srun --gres=gpu:$jobs -n $jobs -p debug  build/src/StandaloneStencilsCUDA
 
