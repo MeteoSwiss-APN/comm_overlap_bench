59570abdc58c50331dfbc4bdf2003b10d75f2f3a
Pascal Spörri <pascal.spoerri@env.ethz.ch>
2017-07-20 12:15:31 +0200

    update tds

--- build_kesch_tds.sh
+++ build_kesch_tds.sh
@@ -5,6 +5,8 @@ mkdir -p build
 module purge
 module load craype-network-infiniband
 module load craype-haswell
+module load craype-accel-nvidia35
+module load cray-libsci
 module load git/2.6.0
 module load tmux/2.1
 module load cudatoolkit/8.0.61
@@ -12,22 +14,21 @@ module load mvapich2gdr_gnu/2.2_cuda_8.0
 module load gcc/5.4.0-2.26
 #module load gcc/4.8.1
 module load cmake/3.7.2
+
 module list -t
 echo
 
-#export LD_LIBRARY_PATH=${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}
 export CC=gcc
 export CXX=g++
-#alias mpicc="/opt/mvapich2/gdr/no-mcast/2.2/cuda8.0/mpirun/gnu4.8.5/bin/mpicc `pkg-config --libs cudart`"
-#alias mpicxx="/opt/mvapich2/gdr/no-mcast/2.2/cuda8.0/mpirun/gnu4.8.5/bin/mpicxx `pkg-config --libs cudart`"
+
 export LDFLAGS=`pkg-config --libs cudart`
-export BOOST_ROOT="/apps/escha/UES/RH6.7/easybuild/software/Boost/1.49.0-gmvolf-15.11-Python-2.7.10"
+export BOOST_ROOT="/apps/escha/UES/jenkins/RH7.3-17.02/easybuild/software/boost/1.63.0-gmvolf-17.02-python-2.7.13/"
 
 pushd build &>/dev/null
     cmake .. \
              -DMPI_VENDOR=mvapich2 \
              -DCUDA_COMPUTE_CAPABILITY="sm_37" \
              -DCMAKE_BUILD_TYPE=Release \
-             -DENABLE_MPI_TIMER=ON
-#             -DBOOST_ROOT="${BOOST_ROOT}" 
+             -DENABLE_TIMER=ON \
+             -DBOOST_ROOT="${BOOST_ROOT}" 
     make -j 1
--- run_kesch_tds.sh
+++ run_kesch_tds.sh
@@ -3,6 +3,8 @@
 module purge
 module load craype-network-infiniband
 module load craype-haswell
+module load craype-accel-nvidia35
+module load cray-libsci
 module load git/2.6.0
 module load tmux/2.1
 module load cudatoolkit/8.0.61
@@ -16,8 +18,8 @@ echo Modules:
 module list -t
 
 # Add the boost library path manually
-#export BOOST_LIBRARY_PATH=/apps/escha/UES/RH6.7/easybuild/software/Boost/1.49.0-gmvolf-15.11-Python-2.7.10/lib
-#export LD_LIBRARY_PATH=$BOOST_LIBRARY_PATH:$LD_LIBRARY_PATH
+export BOOST_LIBRARY_PATH=/apps/escha/UES/jenkins/RH7.3-17.02/easybuild/software/boost/1.63.0-gmvolf-17.02-python-2.7.13/lib
+export LD_LIBRARY_PATH=$BOOST_LIBRARY_PATH:$LD_LIBRARY_PATH
 
 [ -z "${jobs}" ] && jobs=2
 echo "Running with ${jobs} jobs (set jobs environment variable to change)"
@@ -38,18 +40,20 @@ if [ "$jobs" -gt "16" ]; then
     let nodes=($jobs+16-1)/16
 fi
 
-# Setup GPU
+#export G2G=2
+## Setup GPU
 [ -z "${G2G}" ] && export G2G=2
-if [ "${G2G}" == 2 ]; then
-    echo "Setting special settings for G2G=2"
-    export MV2_USE_GPUDIRECT=1
-    export MV2_CUDA_IPC=1
-    export MV2_ENABLE_AFFINITY=1
-    export MV2_GPUDIRECT_GDRCOPY_LIB=/apps/escha/gdrcopy/20170131/libgdrapi.so
-    export MV2_USE_CUDA=1
-    echo
-fi
+#if [ "${G2G}" == 2 ]; then
+#    echo "Setting special settings for G2G=2"
+#    export MV2_USE_GPUDIRECT=1
+#    export MV2_CUDA_IPC=1
+#    export MV2_ENABLE_AFFINITY=1
+#    export MV2_GPUDIRECT_GDRCOPY_LIB=/apps/escha/gdrcopy/20170131/libgdrapi.so
+#    export MV2_USE_CUDA=1
+#    echo
+#fi
 
+export LD_PRELOAD=/opt/mvapich2/gdr/no-mcast/2.2/cuda8.0/mpirun/gnu4.8.5/lib64/libmpi.so
 
 echo "Nodes: ${nodes}"
 echo "Tasks/Node: ${tasks_node}"
@@ -59,7 +63,7 @@ echo "Partition: ${partition}"
 echo =======================================================================
 echo = Default Benchmark
 echo =======================================================================
-srun --nodes=$nodes --ntasks=$jobs --ntasks-per-node=$tasks_node --ntasks-per-socket=$tasks_socket --partition=$partition --gres=gpu:$tasks_node --distribution=block:block --cpu_bind=q  build/src/comm_overlap_benchmark
+srun --nodes=$nodes --ntasks=$jobs --ntasks-per-node=$tasks_node --ntasks-per-socket=$tasks_socket --partition=$partition --gres=gpu:$tasks_node --distribution=block:block --cpu_bind=q   build/src/comm_overlap_benchmark
 echo =======================================================================
 echo = No Communication
 echo =======================================================================
