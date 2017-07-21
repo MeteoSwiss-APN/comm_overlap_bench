3e74b6ff1715f20576a5568871b929139d702328
Pascal Spörri <pascal.spoerri@env.ethz.ch>
2017-07-21 14:14:40 +0200

    Added mvapich2.2 cuda 7.0 sample

---
+++ build_kesch_mvapich2.2.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+rm -rf build_kesch_mvapich22
+mkdir -p build_kesch_mvapich22
+
+module purge
+module load craype-haswell
+module load craype-network-infiniband
+module load mvapich2gdr_gnu/2.2_cuda_7.0
+module load GCC/4.9.3-binutils-2.25
+module load cray-libsci_acc/3.3.0
+
+module load CMake
+module list -t
+echo
+
+export CC=gcc
+export CXX=g++
+export BOOST_ROOT="/apps/escha/UES/RH6.7/easybuild/software/Boost/1.49.0-gmvolf-15.11-Python-2.7.10"
+
+pushd build_kesch_mvapich22 &>/dev/null
+    cmake .. \
+             -DMPI_VENDOR=mvapich2 \
+             -DCUDA_COMPUTE_CAPABILITY="sm_37" \
+             -DCMAKE_BUILD_TYPE=Release \
+             -DENABLE_TIMER=ON \
+             -DBOOST_ROOT="${BOOST_ROOT}" 
+    make -j 1
+
---
+++ run_kesch_mvapich2.2.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+module purge
+module load craype-haswell
+module load craype-network-infiniband
+module load mvapich2gdr_gnu/2.2_cuda_7.0
+module load GCC/4.9.3-binutils-2.25
+module load cray-libsci_acc/3.3.0
+
+module load CMake
+
+module list
+
+# Add the boost library path manually
+export BOOST_LIBRARY_PATH=/apps/escha/UES/RH6.7/easybuild/software/Boost/1.49.0-gmvolf-15.11-Python-2.7.10/lib
+export LD_LIBRARY_PATH=$BOOST_LIBRARY_PATH:$LD_LIBRARY_PATH
+
+[ -z "${jobs}" ] && jobs=2
+echo "Running with ${jobs} jobs (set jobs environment variable to change)"
+
+# Setup Node configuration
+tasks_socket=$jobs
+if [ "$jobs" -gt "8" ]; then
+    tasks_socket=8
+fi
+partition=debug
+tasks_node=$jobs
+if [ "$jobs" -gt "16" ]; then
+    tasks_node=16
+    partition=dev
+fi
+nodes=1
+if [ "$jobs" -gt "16" ]; then
+    let nodes=($jobs+16-1)/16
+fi
+
+# Setup GPU
+[ -z "${G2G}" ] && export G2G=2
+if [ "${G2G}" == 2 ]; then
+    echo "Setting special settings for G2G=2"
+    export MV2_USE_GPUDIRECT=1
+    export MV2_CUDA_IPC=1
+    export MV2_ENABLE_AFFINITY=1
+    export MV2_GPUDIRECT_GDRCOPY_LIB=/apps/escha/gdrcopy/20170131/libgdrapi.so
+    export MV2_USE_CUDA=1
+    echo
+fi
+
+
+echo "Nodes: ${nodes}"
+echo "Tasks/Node: ${tasks_node}"
+echo "Tasks/Socket: ${tasks_socket}"
+echo "Partition: ${partition}"
+
+exe=build_kesch_mvapich22/src/comm_overlap_benchmark
+
+export LD_PRELOAD=/opt/mvapich2/gdr/2.2/cuda7.0/gnu/lib64/libmpi.so
+
+echo =======================================================================
+echo = Default Benchmark
+echo =======================================================================
+srun --nodes=$nodes --ntasks=$jobs --ntasks-per-socket=$tasks_socket --partition=$partition --gres=gpu:$tasks_node --distribution=block:block --cpu_bind=q $exe
+#--ntasks-per-node=$tasks_node 
+
+echo =======================================================================
+echo = No Communication
+echo =======================================================================
+srun --nodes=$nodes --ntasks=$jobs --ntasks-per-socket=$tasks_socket --partition=$partition --gres=gpu:$tasks_node --distribution=block:block --cpu_bind=q $exe --nocomm
+#--ntasks-per-node=$tasks_node 
+
+echo =======================================================================
+echo = No Computation
+echo =======================================================================
+srun --nodes=$nodes --ntasks=$jobs --ntasks-per-socket=$tasks_socket --partition=$partition --gres=gpu:$tasks_node --distribution=block:block --cpu_bind=q  $exe --nocomp
+#--ntasks-per-node=$tasks_node 
+
